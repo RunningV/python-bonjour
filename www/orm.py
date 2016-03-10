fe399166b03884b16bb4b817f4ecf26b8bc79c98
RunningV <1140353684@qq.com>
2016-03-10 21:56:41 +0800

    complete orm module

--- www/orm.py
+++ www/orm.py
@@ -1,11 +1,4 @@
-from orm import Model, StringField, IntegerField
-
-class User(Model):
-  __table__ = 'users'
-
-  id = IntegerField(primary_key=True)
-  name = StringField()
-
+from orm import Model,StringField,IntegerField
 
 @asyncio.coroutine
 def create_pool(loop,**kw):
@@ -13,7 +6,7 @@ def create_pool(loop,**kw):
   global __pool
   __pool = yield from aiomysql.create_pool(
     host = kw.get('host','localhost'),
-    port = kw.get('port',3300),
+    port = kw.get('port',3399),
     user = kw['user'],
     password = kw['password'],
     db = kw['db'],
@@ -22,45 +15,51 @@ def create_pool(loop,**kw):
     maxsize = kw.get('maxsize',10),
     minsize = kw.get('minsize',1),
     loop = loop
-    )
+  )
 
 @asyncio.coroutine
 def select(sql,args,size=None):
   log(sql,args)
   global __pool
-  with(yield from __pool) as con:
-    cur = yield from con.cursor(aiomysql.DictCursor)
+  with(yield from __pool) as conn:
+    cur = yield from conn.cursor(aiomysql.DictCursor)
     yield from cur.execute(sql.replace('?','%s'),args or ())
     if size:
       rs = yield from cur.fetchmany(size)
     else:
       rs = yield from cur.fetchall()
     yield from cur.close()
-    logging.info('rows returned: %s' % len(rs))
+    logging.info('rows returned: %s' %len(rs))
     return rs
 
 @asyncio.coroutine
 def execute(sql,args):
   log(sql)
-  with (yield from __pool) as con:
+  with (yield from __loop) as conn:
     try:
-      cur = yield from con.cursor()
-      yield from cur.execute(sql.replace('?','%s'),args)
+      cur = yield from conn.cursor()
+      yield from cur.execute(sql.replace('?',"%s"),args)
       affected = cur.rowcount
       yield from cur.close()
     except BaseException as e:
       raise
     return affected
 
+class User(Model):
+  __table__ = 'users'
+  id = IntegerField(primary_key=True)
+  name = StringField()
+
+# define Model
 class Model(dict,metaclass=ModelMetaclass):
-  def __init__(self,**kw):
-    super(Model,self).__init__(**kw)
+  def __init__(self, **kw):
+    super(Model, self).__init__(**kw)
 
   def __getattr__(self,key):
     try:
       return self[key]
     except KeyError:
-      raise AttributeError(r"'Model' object has no attribute '%s'" % key)
+      raise AttributeError(r"'Model' object has no attribute '$s'" %key)
 
   def __setattr__(self,key,value):
     self[key] = value
@@ -74,9 +73,25 @@ class Model(dict,metaclass=ModelMetaclass):
       field = self.__mappings__[key]
       if field.default is not None:
         value = field.default() if callable(field.default) else field.default
-        logging.debug('using default value for %s: %s'% (key,str(value)))
+        logging.debug('using default value for %s: %s'%(key,str(value)))
         setattr(self,key,value)
-      return value
+    return value
+
+  @classmethod
+  @asyncio.coroutine
+  def find(cls,pk):
+    rs = yield from select('%s where `%s`=?' %(cls.__select__,cls.__primary_key__),[pk],1)
+    if len(rs) == 0:    
+      return None
+    return cls(**rs[0])
+
+  @asyncio.coroutine
+  def save(self):
+    args = list(map(self.getValueOrDefault,self.__fields__))
+    args.append(self.getValueOrDefault(self.__primary_key__))
+    rows = yield from execute(self.__insert__,args)
+    if rows != 1:
+      logging.warn('failed to insert record: affected rows: %s' %rows)
 
 class Field(object):
   def __init__(self,name,column_type,primary_key,default):
@@ -86,14 +101,42 @@ class Field(object):
     self.default = default
 
   def __str__(self):
-    return '<%s,%s:%s>' % (self.__class__.__name__,self.column_type,self.name)
+    return '<%s, %s:%s>' %(self.__class__.__name__,self.column_type,self.name)
 
 class StringField(Field):
-  def __init__(self,name=None,primary_key=False,default=None,ddl='varchar(100)'):
+  def __init__(self,name=None,primary_key=false,default=None,ddl='varchar(100)'):
     super().__init__(name,ddl,primary_key,default)
 
-class ModelMetaclass(object):
-  def __new__(cls,name,base,attrs):
-    if name = 'Model':
+class ModelMetaclass(type):
+  def __new__(cls,name,bases,attrs):
+    if name=='Model':
       return type.__new__(cls,name,bases,attrs)
-      tableName = attrs.get('__table__',None)
+    tableName = attrs.get('__table__',None) or name
+    logging.info('found model: %s(table: %s)' %(name,tableName))
+
+    mappings = dict()
+    fields = []
+    primaryKey = None
+    for k,v in attrs.items():
+      if isinstance(v,Field):
+        logging.info('found mapping: %s==> %s)' %(k,v))
+        mappings[k] = v
+        if v.primary_key:
+          if primaryKey:
+            raise RuntimeError('Duplicate primary key for field: %s' %k)
+          primaryKey = k
+        else:
+          fields.append(k)
+    if not primaryKey:
+      raise RuntimeError('Primary key not found.')
+    for k in mappings.keys():
+      attrs.pop(k)
+    escaped_fields = list(map(lambda f: '`%s`' %f,fields))
+    attrs['__mappings__'] = mappings
+    attrs['__table__'] = tableName
+    attrs['__primary_key__'] = primaryKey
+    attrs['__fields__'] = fields
+    attrs['__select__'] = 'select `%s`, %s from `%s`' %(primaryKey, ','.join(escaped_fields)),\
+    primaryKey,create_args_string(len(escaped_fields)+1)
+    attrs['__delete__'] = 'delete from `%s` where `%s`=?' %(tableName,primaryKey)
+    return type.__new__(cls,name,bases,attrs)
